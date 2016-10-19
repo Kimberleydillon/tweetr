a02c9e0ee154a53f9f4194d3f62dfae69d6b4c61
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-10-19 02:38:05 +0000

    when 140 limit is exceeded the counter changes to red

--- public/scripts/char-counter.js
+++ public/scripts/char-counter.js
@@ -3,24 +3,9 @@ $(function() {
   $(".new-tweet form textarea").on("keyup", function(event){
     var textarea = $(this)
     var currLength = max - textarea.val().length;
-    $(".new-tweet form span").text(currLength);
+    var till140 = $(".new-tweet form span").text(currLength);
+    if (currLength < 0){
+      $(".new-tweet form span").css({"color":"red"});
+    }
 });
 });
-
-//do max minus counter, dont just process counter.
-
-
-// $(function(){//waiting for DOM to load
-//    alert("Dont shut it!");
-//   $("container").children(".counter").css({"color":"red"});
-
-//   })
-
-
-// $(function(){//waiting for DOM to load
-//   $("button").on"click", function(eventObject){// find a tag named button
-//     var domElement = eventObject.currentTarget;
-//     console.log($(domElement));//special object that represents a dom object on the page
-//     console.log("The button says: " + $(domElement).text());
-//     alert("Hello");
-//   }
